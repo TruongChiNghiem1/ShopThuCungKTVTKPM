425fb4ef08d10cc17e523bb0f89567db217bc8ce
TruongChiNghiem <nghiem0859774418@gmail.com>
2025-02-25 14:33:44 +0700

    Build project

---
+++ backend/service-spring/src/test/java/service_spring/service_spring/ServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package service_spring.service_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
