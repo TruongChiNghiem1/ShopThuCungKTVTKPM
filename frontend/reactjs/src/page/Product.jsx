809febbabd7f410416dccdf6294ffe432f9348c7
TruongChiNghiem <nghiem0859774418@gmail.com>
2025-04-01 20:01:01 +0700

    filter product list

--- frontend/reactjs/src/page/Product.jsx
+++ frontend/reactjs/src/page/Product.jsx
@@ -3,12 +3,14 @@ import ProductList from "../component/Product/ProductList.jsx";
 import Search from "../component/Product/Search.jsx";
 import BlogSection from "../component/home/BlogSection.jsx";
 import Footer from "../component/home/Footer.jsx";
+import ProductFilter from "../component/Product/ProductFilter.jsx";
 
 function Product() {
     return (
         <div>
             <Navbar />
             <Search/>
+
             <ProductList />
             <BlogSection />
             <Footer />
